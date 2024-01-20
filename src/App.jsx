7a6778ffb2f3a7add6b4c2a8117cf3b111d9991f
cosmeNicolas <nicomas7590@gmail.com>
2024-01-20 16:03:34 -0300

    inicialice el proyecto TeaviseAppWeb

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import {Footer}  from './components/Footer';
+import {Container} from 'react-bootstrap'
+
+function App() {
+  
+
+  return (
+    <>
+     <Container className="main" >
+       
+     </Container>
+     <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <footer className="py bg bg-dark">
+      <p className="text-light text-center">
+        &copy; Todos los derechos reservados
+      </p>
+    </footer>
+  );
+};
